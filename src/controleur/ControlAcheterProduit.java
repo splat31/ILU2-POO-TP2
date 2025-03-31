65206357374c66c5bd1fca11b995880b2e0b8474
splat31 <bastien31800@orange.fr>
2025-03-31 22:19:27 +0200

    fin tp manque traiter cas acheteur achete a lui meme

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,6 +1,8 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Village;
+import villagegaulois.Etal;
 
 public class ControlAcheterProduit {
 	private Village village;
@@ -15,5 +17,41 @@ public class ControlAcheterProduit {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	public String[] TrouverProduit(String nomAcheteur,String produit) {
+		if (controlVerifierIdentite.verifierIdentite(nomAcheteur)) {
+			Gaulois[] tab =village.rechercherVendeursProduit( produit);
+			if (tab==null) {
+				
+				String[] tabretour= new String[1];
+				tabretour[0]="pas de vendeur";
+				return tabretour;
+			} else {
+				
+				String[] tabretour=new String[tab.length];
+				for (int i=0;i<tab.length;i++) {
+					tabretour[i]=tab[i].getNom();
+					
+				}
+				return tabretour;
+			}
+		} else {
+			String[] tabretour= new String[1];
+			tabretour[0]="pas dans le village";
+			return tabretour;
+		}
+	}
+	
+	public int AcheterProduit(String nomVendeur, String Produit, int demande) {
+		Gaulois gaulois = village.trouverHabitant(nomVendeur);
+		Etal etal = recherche(gaulois);
+		int vendu=etal.acheterProduit(demande);
+		return vendu;
+	}
+	
+	public Etal recherche(Gaulois vendeur) {
+		Etal etal = village.rechercherEtal(vendeur);
+		return etal;
+	}
+	
+	
 }
